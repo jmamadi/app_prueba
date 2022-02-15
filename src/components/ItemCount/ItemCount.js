695812bc0c8532a566ef2efc8b62a93cf1043285
Juan Martin Amadi Dattero <jjmamadi@gmail.com>
2022-02-14 23:13:42 -0300

    inserted counter

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+const ItemCount = ({stock = 13, initial = 1, onAdd})=> {
+   const [quantity, setQuantity] = useState(initial)
+
+   const increment = () => {
+       if(quantity < stock) {
+           setQuantity(quantity+1)
+       }
+   }
+
+   const decrement = () => {
+       if(quantity > 0) {
+           setQuantity(quantity - 1)
+       }
+   }
+
+   return(
+       <div align="center">
+            <h2> Cafe Colombiano</h2>
+            <td align="center" style={{fontSize : '1rem'}}>{quantity}</td>
+                <table >
+                    <tbody>
+                        <tr>
+                            <td align="left"><button className="Option" onClick={()=> decrement() }>-</button></td>
+                            <td align="center" colSpan="5"><button className="Option" onClick={() => onAdd(quantity)}>Agregar al carrito</button></td>                            
+                            <td align="right"><button className="Option" onClick={() => increment() }>+</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+       </div>
+   )
+
+}
+
+export default ItemCount
