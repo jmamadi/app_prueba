d958623812cd1fbd64ebda3a17ded3415d9a03b0
Juan Martin Amadi Dattero <jjmamadi@gmail.com>
2022-02-21 23:19:23 -0300

    detail v0

--- src/mock/products.js
+++ src/mock/products.js
@@ -1,48 +1,15 @@
 export const products = [
-    {
-        id: 1,
-        name: 'Cafe Colombiano',
-        stock: 15,
-        price: 2000,
-        //img: './img/camisa.jpg',
-        img: './images/bandera_1.png',
-        category: 'Cafe',
-    },
-    {
-        id: 2,
-        name: 'Cafe Brasilero',
-        stock: 20,
-        price: 2500,
-        img: './images/bandera_2.png',
-        category: 'Cafe',
-    },
-    {
-        id: 3,
-        name: 'Cafe Costaricence',
-        stock: 15,
-        price: 2700,
-        img: './images/bandera_3.png',
-        category: 'Cafe',
-    },
-    {
-        id: 4,
-        name: 'Cafe Indú',
-        stock: 8,
-        price: 3000,
-        //img: './img/rinonera.jpg',
-        img: './images/bandera_4.png',
-        category: 'Cafe',
-    },
+    { id: 1, name: 'Cafe Colombiano', stock: 15, price: 2000, img: './images/bandera_1.png', descripcion: 'Se caracteriza por tener mucho cuerpo, notas de dulce, nuez y chocolate, hasta florales y frutales, casi tropical', region: 'Antioquía', category: 'Cafe'},
+    { id: 2, name: 'Cafe Brasilero', stock: 20, price: 2500, img: './images/bandera_2.png', descripcion: 'Excelente café, de aroma suave, cuerpo ligero y sabor dulce', category: 'Cafe'},
+    { id: 3, name: 'Cafe Costaricence', stock: 15, price: 2700, img: './images/bandera_3.png', descripcion: 'De las variedades Caturra , que produce un grano de mayor calidad y con mejores características aromáticas', category: 'Cafe'},
+    { id: 4, name: 'Cafe Indú', stock: 8, price: 3000, img: './images/bandera_4.png', descripcion: 'Su taza es intensamente aromática, de sabor suave y con notas de chocolate', category: 'Cafe'},
+    { id: 5, name: 'Taza Personalizada', stock: 12, price: 2800, img: './images/tazapersonalizada_1.png', descripcion: 'Producto de porcelana, con una película templada totalmente personalizable', category: 'Tazas'},
+    { id: 6, name: 'Desayuno Premium', stock: 13, price: 3000, img: './images/desayuno.png', descripcion: 'En ese día tan especial, regalale la mejor sorpresa a es ser que tanto queres', category: 'Desayunos'},
 ];
+
 
 export const traerProductos = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve(products);
-    }, 2000);
-});
-
-export const traerProducto = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(products[0]);
     }, 2000);
 });
