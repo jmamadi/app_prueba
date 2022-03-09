5bed912de0610f8c76d93569bd1c54231c702afe
Juan Martin Amadi Dattero <jjmamadi@gmail.com>
2022-03-09 06:01:24 -0300

    context con cartw

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,37 @@
-import React, { useContext } from 'react';
-import Context, { CartContextProvider } from '../../context/CartContext';
+import { useContext } from "react"
+import CartContext from '../../context/CartContext'
+import { useNotificationServices } from "../../services/notification/NotificationServices"
 
 const Cart = () => {
-//    const { cart } = useContext(Context);
+    const { products, removeItem, getTotal } = useContext(CartContext)
+
+    const setNotification = useNotificationServices()
+
+    if(products.length === 0) {
+        return <h1>No hay productos en el carrito</h1>
+    }
+
+    const handleRemoveItem = (id, name) => {
+        removeItem(id)
+        setNotification('success', `Se removio ${name} del carrito`)
+    }
 
     return (
-        <h1>Cart</h1>
-/*        <>
-            {cart.map((prod) => (
-                <li key={prod.id}>{prod.cantidad}</li>
-            ))}
+        <>
+            <h1>Cart</h1>
+            {
+                products.map(prod => {
+                    return (
+                        <div key={prod.id} style={{ display: 'flex'}}>
+                            <h3>{prod.name}</h3>
+                            <h3>Cantidad {prod.quantity}</h3>
+                            <button onClick={() => handleRemoveItem(prod.id, prod.name)}>X</button>
+                        </div>
+                    )
+            })}
+            <h1>Total: ${getTotal()}</h1>
         </>
-*/
-    );
-};
+    )
+}
 
-export default Cart;
+export default Cart
--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,11 +1,17 @@
 import './CartWidget.css'
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import CartContext from '../../context/CartContext'
 
 const CartWidget = () => {
+
+    const { getQuantity } = useContext(CartContext)
+
     return(
-        <button className="CartWidget">
+        <Link to={'/cart'} className="CartWidget">
             <img src="/images/cart.svg" alt='cart' className='CartImg'/>
-            0
-        </button>
+            {getQuantity()}
+        </Link>
     );
 }
 
