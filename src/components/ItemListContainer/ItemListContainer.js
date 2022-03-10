18931ba2befd6e92f9caa18ead7060ccfa04fda7
Juan Martin Amadi Dattero <jjmamadi@gmail.com>
2022-03-10 00:17:22 -0300

    contextsubida

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,48 +1,54 @@
-import { useEffect, useState } from 'react';
-import ItemList from '../ItemList/ItemList';
-import { getProducts } from '../../mock/products';
-import { useParams } from 'react-router-dom';
+import './ItemListContainer.css'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { getDocs, collection, query, where } from 'firebase/firestore'
+import { firestoreDb } from '../../services/firebase/firebase'
+import ItemList from '../ItemList/ItemList'
 import { useNotificationServices } from '../../services/notification/NotificationServices'
 
 const ItemListContainer = () => {
-    const [products, setProducts] = useState([]); /* [] */
+    const [products, setProducts] = useState([])
     const [loading, setLoading] = useState(true)
     const { categoryId } = useParams()
 
-    const [title, setTitle] = useState('')
-
-    // const setNotification = useNotificationServices()
+    const setNotification = useNotificationServices()
 
     useEffect(() => {
-        // setNotification('error', 'Bienvenido')
         setLoading(true)
 
-        getProducts(categoryId).then(item => {
-                setProducts(item);
-            }).catch((err) => {
-                console.log(err);
-            }).finally(() => {
-                setLoading(false)
+        const collectionRef = categoryId ?
+            query(collection(firestoreDb, 'products'), where('category', '==', categoryId)) :
+            collection(firestoreDb, 'products')
+
+        getDocs(collectionRef).then(response => {
+            const products = response.docs.map(doc => {
+                return { id: doc.id, ...doc.data() }
             })
 
-            setTimeout(() => {
-                setTitle('Categoría')
-            }, 2000)
+            setProducts(products)
+        }).catch((error) => {
+            setNotification('error',`Error buscando productos: ${error}`)
+        }).finally(() => {
+            setLoading(false)
+        })
 
-            return (() => {
-                setProducts()
-            })
 
-    }, [categoryId]);
+        return (() => {
+            setProducts()
+        })
+    }, [categoryId]) // eslint-disable-line
 
     return (
         <div>
-            {title}
             {
-                loading ? <h1>Cargando...</h1> : products.length ? <ItemList products={products}/> : <h1>No se encontraron productos!</h1>
+                loading ?
+                    <h1>Cargando...</h1> :
+                products.length ?
+                    <ItemList products={products}/> :
+                    <h1>No se encontraron productos!</h1>
             }
         </div>
-    );
-};
+    )
+}
 
-export default ItemListContainer;
+export default ItemListContainer
---
+++ src/services/firebase/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUVLGEBbj6RGJf-QhJ9hMO4ZeQMERmtCU",
+  authDomain: "camada-24925-4208f.firebaseapp.com",
+  projectId: "camada-24925-4208f",
+  storageBucket: "camada-24925-4208f.appspot.com",
+  messagingSenderId: "1017622677361",
+  appId: "1:1017622677361:web:73a1cce726dcb3d36218d0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+export const firestoreDb = getFirestore(app)
